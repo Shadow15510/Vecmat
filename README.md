d027e63a69aa52b81ec0f292084bad4122c54972
Sha-Chan <antoine.royer31@orange.fr>
2020-06-24 17:25:51 +0200

    Adding the README and some functions

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # Vecmat
-Librairie of vectorial and matricial functions
+
+## General presentation
+
+### Presentation
+
+Vecmat is a librairie of vectorial and matricial manipulations. This librairie is oriented-object, so you can use `vector` and `matrix` like `list` or `str` objects.
+
+### Licence
+
+This code is provided with licence (CC BY-NC-SA 4.0). For more information about licence please [https://creativecommons.org/licenses/by-nc-sa/4.0/](click here).
+
+## Vector's manipulations
+
+### Generalities
+
+The `Vector` objets was designed for support n dimensions. Such as `u = Vector(1, 1)` creates a two-dimensional vector, or even `u = Vector(1, 2, 3, 4, 5)` creates a five-dimensional vector. (Don't forget the dot for use the handlings, for instance : `my_vector.show()`.)
+
+### Vectorial manipulations available
+
+#### Handling on the vector itself
+
+ - `show()` : Displays the vector's coordinates.
+ - `norm()` : Returns the vector's norm.
+ - `unitV()` : Returns the unitary vector.
+
+#### Handling on several vectors
+
+ - `dotP(vec)` : Returns the dot product.
+ - `crossP(vec)` : Returns the cross product. (Only available for two three-dimensions vectors.)
+ - `det(*vec)` : Returns the vectors's determinant. You can pass several vectors without any problem. (Please take care to pass n vectors to n dimensions.)
+ - `colinear(vec)` : Tests the colinearty between the vectors. Return a boolean.
+ - `angle(vec)` : Returns the angle in degrees between the vectors.
+
+## Matrix's manipulations
+
+### Generalities
+
+`Matrix` was designed for support n * m dimensions, so you don't have to take care at dimensions. If an error occured, it's because the dimensions doesn't allow to calculate what you want. For exemple, the matrix's determinant is only available with squarred matrix. As well as for `Vector`, don't forget the dot.
+
+### Matricial manipulations available
+
+#### Basical handlings
+
+ - `show()` : Displays the matrix to the shell row by row.
+ - `get_coef(i, j)` : Returns the i,j coefficient of the matrix. (Please take care, as well as for list, the index start at 0.)
+ - `get_dim()` : Returns the dimension of the matrix. The results is a tuple : `(row, column)`.
+
+#### Handlings on the matrix itself
+
+ - `switch_row(row_1, row_2)` : Reverses the two rows.
+ - `switch_column(column_1, column_2)` : Reverses the two columns.
+ - `write_row(index, new_row)` : Replaces the index-row by the new row
+ - `write_column(index, new_column)` : Replaces the index-column by the new column 
+
+#### Basical mathematical operations
+
+ - `plus(mat)` : Adds the two matrixs and returns a `Matrix` object.
+ - `minus(mat)` : Substracts the two matrixs and returns a `Matrix` object.
+ - `times_mat(mat)` : Multiplies the two matrixs and returns a `Matrix` object.
+ - `times_nb(nb)` : Multiplies by a number and returns a `Matrix` object.
+ - `by_mat(mat)` : Divides the two matrixs and returns a `Matrix` object.
+ - `by_nb(nb)` : Divides by a number and returns a `Matrix` object.
+
+#### Advanced matricial manipulations
+
+ - `augment(mat)` : Allows to augment the size of the matrix by adding another matrix to the first one.
+ - `sub(row_st, column_st, row_ed, column_ed)` : Returns a sub-matrix between the given limits.
+ - `det()` : Returns the matrix's determinant.
+ - `tranpose()` : Returns the transpose matrix.
+ - `s_mat(jmp_i, jmp_j)` : Returns the matrix without the i-row and j-column.
+ - `comat()` : Returns the co-matrix.
+ - `inverse()` : Returns the inverse matrix. (Please take care to the determinant.)
---
+++ vecmat.py
@@ -0,0 +1,117 @@
+# --------------------------------------------------
+# Vecmat
+# by Sha-Chan~
+# last version : 1.0 on June 2020
+#
+# Code provided with licence (CC BY-NC-SA 4.0)
+# for more information about licence :
+# https://creativecommons.org/licenses/by-nc-sa/4.0/
+# --------------------------------------------------
+
+
+from math import *
+
+class Vector:
+  def __init__(self, *coord):
+    self.coord = list(coord)
+    self.dim = len(coord)
+      
+  def show(self):
+    print(self.coord)
+    
+  def norm(self):
+    return sqrt(sum([i**2 for i in self.coord]))
+
+  def unitV(self):
+    return Vector(*[i / self.norm() for i in self.coord])
+    
+  def dotP(self, vec):
+    return sum([self.coord[i] * vec.coord[i] for i in range(len(self.coord))])
+  
+  def crossP(self, vec):
+    if self.dim == 3 and vec.dim == 3: return Vector(self.z*vec.y - self.z*vec.z, self.x*vec.z - self.z*vec.x, self.y*vec.x - self.x*vec.y)
+
+  def det(self, *vec):
+    return Matrix([self.coord] + [i.coord for i in vec]).det()
+  
+  def colinear(self, vec):
+    return not self.det(vec)
+
+  def angle(self, vec):
+    return round(degrees(acos(self.dotP(vec) / (self.norm()*vec.norm()))), 2)
+
+class Matrix:
+  def __init__(self, content):
+    self.content = content
+
+  def show(self):
+    for i in range(len(self.content)): print(self.content[i])
+  
+  def get_coef(self, i, j):
+    return self.content[i][j]
+    
+  def get_dim(self):
+    return len(self.content), len(self.content[0])
+    
+  def plus(self, mat):
+    return Matrix([[self.content[i][j] + mat.content[i][j] for j in range(len(self.content[0]))] for i in range(len(self.content))])
+    
+  def minus(self, mat):
+    return Matrix([[self.content[i][j] - mat.content[i][j] for j in range(len(self.content[0]))] for i in range(len(self.content))])
+ 
+  def times_mat(self, mat):
+    return Matrix([[sum([self.content[j][i] * mat.content[i][j] for i in range(len(self.content[0]))]) for k in range(len(mat.content[0]))] for j in range(len(self.content))])
+ 
+  def times_nb(self, nb):
+    return Matrix([[self.content[i][j] * nb for j in range(len(self.content[0]))] for i in range(len(self.content))])
+    
+  def by_mat(self, mat):
+    return self.times_mat(mat.inverse())
+    
+  def by_nb(self, nb):
+    return Matrix([[self.content[i][j] / nb for j in range(len(self.content[0]))] for i in range(len(self.content))])
+    
+  def augment(self, mat):
+     [self.content[i].append(mat.content[i][j]) for j in range(len(self.content[0])) for i in range(len(self.content))]
+        
+  def sub(self, row_st, column_st, row_ed, column_ed):
+    return Matrix([[self.content[i][j] for j in range(column_st, column_ed+1)] for i in range(row_st, row_ed+1)])
+  
+  def det(self):
+    def calc_det(mat):
+      rslt = 0
+      if mat.get_dim() == (1, 1): rslt += mat.content[0][0]
+      else:
+        for i in range(len(mat.content[0])): rslt += (mat.content[0][i], -mat.content[0][i])[i % 2] * calc_det(mat.s_mat(i, 0))
+      return rslt
+    return calc_det(self)
+    
+  def transpose(self):
+    return Matrix([[self.content[i][j] for i in range(len(self.content))] for j in range(len(self.content[0]))])
+  
+  def s_mat(self, jmp_i, jmp_j):
+    return Matrix([[self.content[j][i] for i in range(len(self.content)) if i != jmp_i] for j in range(len(self.content[0])) if j != jmp_j])  
+
+  def comat(self):
+    return Matrix([[(-1) ** (i + j) * self.s_mat(i, j).det() for i in range(len(self.content))] for j in range(len(self.content[0]))])
+
+  def inverse(self):
+    return self.comat().transpose().times_nb(1 / self.det())
+
+  def switch_row(self, row_1, row_2):
+    for j in range(len(self.content[0])): self.content[row_1][j], self.content[row_2][j] = self.content[row_2][j], self.content[row_1][j]
+
+  def switch_column(self, column_1, column_2):
+    for i in range(len(self.content)): self.content[i][column_1], self.content[i][column_2] = self.content[i][column_2], self.content[i][column_1]
+    
+  def write_row(self, index, new_row):
+    for j in range(len(self.content[0])): self.content[index][j] = new_row.content[0][j]
+
+  def write_column(self, indef, new_column):
+    for i in range(len(self.content)): self.content[i][index] = new_column.content[i][0]
+    
+def identity(n):
+  return Matrix([[(0,1)[i == j] for i in range(n)] for j in range(n)])
+    
+    
+    
